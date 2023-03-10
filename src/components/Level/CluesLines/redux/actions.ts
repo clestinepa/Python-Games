70a21232f878c445b6d68a883661f513c5bb7a40
Célestine PAILLE <celestine.paille@soprasteria.com>
2023-03-10 17:13:03 +0100

    add action to change class CluesLines

--- src/components/Level/CluesLines/redux/actions.ts
+++ src/components/Level/CluesLines/redux/actions.ts
@@ -1,11 +1,34 @@
 import { createAction } from "@reduxjs/toolkit";
 import { Level } from "../../interfaces/LevelSource";
 
-export const initialCluesLines = createAction(
-  "cluesLines/initialCluesLines",
-  function prepare(level: Level) {
+export const initialCluesLines = createAction("cluesLines/initialCluesLines", function prepare(level: Level) {
     return {
-      payload: { level },
+        payload: { level },
     };
-  }
-);
+});
+
+export const cluesLineNormal = createAction("cluesLines/cluesLineNormal", function prepare(nb_line: number) {
+    return { payload: { nb_line } };
+});
+
+export const cluesLineDone = createAction("cluesLines/cluesLineDone", function prepare(nb_line: number) {
+    return { payload: { nb_line } };
+});
+
+export const cluesLineError = createAction("cluesLines/cluesLineError", function prepare(nb_line: number) {
+    return { payload: { nb_line } };
+});
+
+// function autoCrossLine(nb_line) {
+//   currentBoard[nb_line] = currentBoard[nb_line].map(function (v) { return v == 0 ? 2 : v });
+//   for (let zone of zones) {
+//     if (parseInt(zone.classList[2].replace("line_", "")) == nb_line && zone.classList[1] == "zone_empty") {
+//       zone.className = zone.className.replace("zone_empty", "zone_cross");
+//     }
+//   }
+// }
+
+// /**TODO */
+// function autoCrossZone(nb_line) {
+//   console.log("to do")
+// }
--- src/components/Level/CluesLines/redux/reducers.ts
+++ src/components/Level/CluesLines/redux/reducers.ts
@@ -1,25 +1,49 @@
 import { createReducer } from "@reduxjs/toolkit";
 import { CluesLinesState } from "../../interfaces/LevelState";
-import { initialCluesLines } from "./actions";
-
+import { cluesLineDone, cluesLineError, cluesLineNormal, initialCluesLines } from "./actions";
 
 const initialState: CluesLinesState = {
-    classCluesLines: []
-
+    classCluesLines: [],
 };
 
 export const cluesLinesReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(initialCluesLines, (state, action) => {
-      for (let nb_line = 0; nb_line < action.payload.level.size; nb_line++) {
-        let cluesLineClass: string[] = [];
-        for (let nb_clue = 0; nb_clue < action.payload.level.clues.line.length;nb_clue++) {
-          cluesLineClass.push("clue_normal");
-        }
-        state.classCluesLines.push({
-          classGlobal: "clues_normal",
-          classClues: cluesLineClass,
+    builder
+        .addCase(initialCluesLines, (state, action) => {
+            for (let nb_line = 0; nb_line < action.payload.level.size; nb_line++) {
+                let cluesLineClass: string[] = [];
+                for (let nb_clue = 0; nb_clue < action.payload.level.clues.line.length; nb_clue++) {
+                    cluesLineClass.push("clue_normal");
+                }
+                state.classCluesLines.push({
+                    classGlobal: "clues_normal",
+                    classClues: cluesLineClass,
+                });
+            }
+        })
+        .addCase(cluesLineNormal, (state, action) => {
+            state.classCluesLines[action.payload.nb_line].classGlobal = "clues_normal";
+            for (let nb_clue = 0; nb_clue < state.classCluesLines[action.payload.nb_line].classClues.length; nb_clue++) {
+                state.classCluesLines[action.payload.nb_line].classClues[nb_clue] = "clue_normal";
+            }
+        })
+        .addCase(cluesLineDone, (state, action) => {
+            state.classCluesLines[action.payload.nb_line].classGlobal = "clues_done";
+            for (let nb_clue = 0; nb_clue < state.classCluesLines[action.payload.nb_line].classClues.length; nb_clue++) {
+                state.classCluesLines[action.payload.nb_line].classClues[nb_clue] = "clue_done";
+            }
+        })
+        .addCase(cluesLineError, (state, action) => {
+            state.classCluesLines[action.payload.nb_line].classGlobal = "clues_ERROR";
+            for (let nb_clue = 0; nb_clue < state.classCluesLines[action.payload.nb_line].classClues.length; nb_clue++) {
+                state.classCluesLines[action.payload.nb_line].classClues[nb_clue] = "clue_ERROR";
+            }
         });
-      }
-    });
 });
+
+// function displayCluesLineError(nb_line) {
+//   clues_lines.children[nb_line].className = clues_lines.children[nb_line].className.replace("clues_normal", "clues_ERROR");
+//   for (let clue_ERROR of clues_lines.children[nb_line].children) {
+//     console.log(clue_ERROR.id)
+//     clue_ERROR.className = clue_ERROR.className.replace("clue_normal", "clue_ERROR");
+//   }
+// }
