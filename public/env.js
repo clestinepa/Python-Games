431c996e804731eb2e6cb402e0635dc5b40474b2
Clemestine <celestinepaille@gmail.com>
2025-02-07 11:17:58 +0100

    ⚰️ delete everything

--- public/env.js
+++
@@ -1,7 +0,0 @@
-window.globalData = {
-  // REACT_APP_ON_GAME: false,
-  // REACT_APP_GAME_ON_ACTION: false,
-  // REACT_APP_GAME_ON_FILL: true,
-  // REACT_APP_GAME_ON_CROSS: false,
-  // REACT_APP_GAME_ON_EMPTY: false,
-};
--- src/components/App/index.tsx
+++
@@ -1,8 +0,0 @@
-import Game from "../Game";
-import "../../styles/global.css";
-
-const App: React.FC = () => {
-  return <Game />;
-};
-
-export default App;
